88af3a01b2280901dc1f885a605d1cdeaacff5d5
Shivani Bhatt <shivanibhatt204@gmail.com>
2026-02-04 19:19:31 +0530

    Fixed base url in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hrms-lite-tgrv.onrender.com/api/",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
